0dfda434661a33e3107867242635e07d6cfb1240
Yang-Seungmin <tmdals099@gmail.com>
2022-08-06 00:23:55 +0900

    Add markdown block

---
+++ src/component/post/blocks/normal/TopInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function TopInfo({title, name, time, favorites}) {
+    return (
+        <div>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,500,0,0"/>
+
+            <div className="row align-items-baseline">
+                <div className="col-auto">
+                    <h3 className="fw-bold">{title}</h3>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-auto text-secondary">
+                    <span className="material-symbols-outlined align-text-top" style={{fontSize: "110%"}}>person</span> {name}
+                </div>
+                <div className="col-auto text-secondary">
+                    <span className="material-symbols-outlined align-text-top" style={{fontSize: "110%"}}>event</span> {time}
+                </div>
+                <div className="col-auto text-primary">
+                    <p><span className="material-symbols-outlined align-text-top" style={{fontSize: "110%"}}>thumb_up</span> <span>{favorites}</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
