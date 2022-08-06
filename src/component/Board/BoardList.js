524700e909bf934beee41f81806d1a29cea8397f
GyuBa <sansam1010@naver.com>
2022-08-07 05:39:58 +0900

    add loading state, ConstantValue.js

--- src/component/Board/BoardList.js
+++ src/component/Board/BoardList.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {ListGroup} from "react-bootstrap";
-
+import instance from "../../ConstantValue";
 export default function BoardList() {
     const url = "http://127.0.0.1:8000/post/list/"
     const [data, setData] = useState([]);
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true)
     const token = "AAAAAAAAAA";
 
     // useEffect(() => {
@@ -25,33 +26,45 @@ export default function BoardList() {
         let completed = false;
 
         async function get(){
-            const result = await axios.get('http://127.0.0.1:8000/post/list/', { headers: {'Authentication' : `Bearer ${token}`}})
+            const result = await instance.get("/post/list", { headers: {'Authentication' : `Bearer ${token}`}})
 
-            .then(function (response){
-                return response
-            })
-            .catch(function (error){
-                console.log("ERROR");
-                setError(true)
-                console.log(error)
-                return error.response;
-                // ERROR
-            });
-            if(!completed)
+                .then(function (response){
+                    // console.log(response.data);
+                    return response
+                })
+                .catch(function (error){
+                    console.log("ERROR");
+                    setError(true)
+                    console.log(error)
+                    return error.response;
+                    // ERROR
+                });
+
+            if(!completed) {
+                setLoading(false);
                 setData(result.data);
+                console.log(data);
+            }
         }
-
-        get().then(() => (console.log("success")));
-
+        get();
 
         return () => {completed = true}
     }, []);
 
-
-    if(!error) {
+    if(loading){
+        return (<ListGroup className="rounded-3 shadow-lg mt-4">
+                <ListGroup.Item key={-1} className="ps-4">
+                    <h4 className="mt-3"> </h4>
+                    <p>불러오는 중입니다.</p>
+                    <p className="text-primary" style={{fontSize: "0.8rem"}}> </p>
+                </ListGroup.Item>
+            </ListGroup>
+        )
+    }
+    else if(!error && !loading) {
         return (
             <ListGroup className="rounded-3 shadow-lg mt-4">
-                {data.map((item) => (
+                {data['data'].map((item) => (
                     <ListGroup.Item key={item.id} className="ps-4">
                         <h4 className="mt-3">{item.title}</h4>
                         <p>{item.body}</p>
@@ -68,7 +81,7 @@ export default function BoardList() {
                     <p>게시물이 없습니다.</p>
                     <p className="text-primary" style={{fontSize: "0.8rem"}}> </p>
                 </ListGroup.Item>
-        </ListGroup>
+            </ListGroup>
         )
     }
 };
