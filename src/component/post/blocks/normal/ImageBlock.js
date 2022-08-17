565ebd72be71901c1d3e9276d4f3287879e9aaa3
Yang-Seungmin <tmdals099@gmail.com>
2022-08-17 20:39:17 +0900

    Add react routing

--- src/component/post/blocks/normal/ImageBlock.js
+++ src/component/post/blocks/normal/ImageBlock.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function ImageBlock({src, description}) {
     return (
-        <div className="text-center">
-            <img className="rounded-3 shadow" src={src} style={{maxHeight: "50vh"}} />
+        <div className="text-center mb-4">
+            <img className="rounded-3 shadow" src={src} style={{maxWidth:"90vh", maxHeight: "50vh"}} />
             <p className="pt-2 small text-secondary">{description}</p>
         </div>
     )
