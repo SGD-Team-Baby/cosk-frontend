6045067a3a86630a8f117558528e27b72cf8ded3
GyuBa <sansam1010@naver.com>
2022-08-25 22:22:34 +0900

    Check Login

--- src/service/user/userImpormation.js
+++ src/service/user/userImpormation.js
@@ -1,16 +1,19 @@
 import {useEffect, useState} from "react";
 import instance from "../../ConstantValue";
 import {getToken} from "../TokenService";
+import {isLogined} from "./LoginService";
 export default function useGetUserInpormation() {
     const [user,setUser] = useState({id:0, name:"",question:0, answer:0})
     const token = getToken();
 
     useEffect(() => {
-
-        instance.get(`/account/user`, {headers: {'Authorization': "Bearer " + token}})
-            .then(function (response){
-                setUser(response.data);
-            })
+        if(isLogined()) {
+            instance.get(`/account/user`, {headers: {'Authorization': "Bearer " + token}})
+                .then(function (response) {
+                    setUser(response.data);
+                    console.log(response.data.id)
+                })
+        }
 
     }, [])
 
